b0bfd3850d685d95a54d2148ca67891b3df3e85a
Cedric K. Wachira <cedric.kiama@gmail.com>
2024-02-24 11:44:17 +0400

    More Practice

---
+++ my-react-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+import './styles.css';
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
