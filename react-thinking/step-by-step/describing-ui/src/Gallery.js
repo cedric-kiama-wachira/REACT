76278c285a91de90b48052f698984205f8d58b5a
Cedric K. Wachira <cedric.kiama@gmail.com>
2024-02-26 13:37:57 +0400

    JSX and Defining Components

---
+++ react-thinking/step-by-step/describing-ui/src/Gallery.js
@@ -0,0 +1,14 @@
+import Profile  from "./Profile.js"
+
+export default function Gallery(){
+    return(
+        <div>
+            <section>
+                <h1>Amazing Scientist</h1>
+                <Profile />
+                <Profile />
+                <Profile />
+            </section>
+        </div>
+    )
+}
---
+++ react-thinking/step-by-step/describing-ui/src/Profile.js
@@ -0,0 +1,17 @@
+export default function Profile(){
+    return(
+<>
+  <h1>Hedy Lamarr's Todos</h1>
+  <img
+    src='file:///home/timeline/Coding/JavaScript/my-image/Cedric.jpg'
+    alt="Hedy Lamarr"
+    className="photo"
+  />
+  <ul>
+    <li>Invent new traffic lights</li>
+    <li>Rehearse a movie scene</li>
+    <li>Improve the spectrum technology</li>
+  </ul>
+</>
+    )
+}
