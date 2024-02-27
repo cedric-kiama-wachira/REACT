e13f26c9f967900787857ffa317e30d882d431ab
Cedric K. Wachira <cedric.kiama@gmail.com>
2024-02-27 20:39:35 +0400

    More on conditional rendering

--- react-thinking/step-by-step/conditional-renderning/src/App.js
+++ react-thinking/step-by-step/conditional-renderning/src/App.js
@@ -1,36 +1,165 @@
-function Item({ name, importance }) {
-  return (
-    <li className="item">
-      {name}
-      {importance > 0 && ' '}
-      {importance > 0 &&
-        <i>(Importance: {importance})</i>
-      }
+// function Item({ name, importance }) {
+//   return (
+//     <li className="item">
+//       {name}
+//       {importance > 0 && ' '}
+//       {importance > 0 &&
+//         <i>(Importance: {importance})</i>
+//       }
+//     </li>
+//   );
+// }
+
+// export default function PackingList() {
+//   return (
+//     <section>
+//       <h1>Sally Ride's Packing List</h1>
+//       <ul>
+//         <Item 
+//           importance={9} 
+//           name="Space suit" 
+//         />
+//         <Item 
+//           importance={0} 
+//           name="Helmet with a golden leaf" 
+//         />
+//         <Item 
+//           importance={6} 
+//           name="Photo of Tam" 
+//         />
+//       </ul>
+//     </section>
+//   );
+// }
+
+// function Drink({ name }) {
+//   return (
+//     <section>
+//       <h1>{name}</h1>
+//       <dl>
+//         <dt>Part of plant</dt>
+//         <dd>{name === 'tea' ? 'leaf' : 'bean'}</dd>
+//         <dt>Caffeine content</dt>
+//         <dd>{name === 'tea' ? '15–70 mg/cup' : '80–185 mg/cup'}</dd>
+//         <dt>Age</dt>
+//         <dd>{name === 'tea' ? '4,000+ years' : '1,000+ years'}</dd>
+//       </dl>
+//     </section>
+//   );
+// }
+
+// export default function DrinkList() {
+//   return (
+//     <div>
+//       <Drink name="tea" />
+//       <Drink name="coffee" />
+//     </div>
+//   );
+// }
+
+// function Drink({ name }) {
+//   let part, caffeine, age;
+//   if (name === 'tea') {
+//     part = 'leaf';
+//     caffeine = '15–70 mg/cup';
+//     age = '4,000+ years';
+//   } else if (name === 'coffee') {
+//     part = 'bean';
+//     caffeine = '80–185 mg/cup';
+//     age = '1,000+ years';
+//   }
+//   return (
+//     <section>
+//       <h1>{name}</h1>
+//       <dl>
+//         <dt>Part of plant</dt>
+//         <dd>{part}</dd>
+//         <dt>Caffeine content</dt>
+//         <dd>{caffeine}</dd>
+//         <dt>Age</dt>
+//         <dd>{age}</dd>
+//       </dl>
+//     </section>
+//   );
+// }
+
+// export default function DrinkList() {
+//   return (
+//     <div>
+//       <Drink name="tea" />
+//       <Drink name="coffee" />
+//     </div>
+//   );
+// }
+
+
+// const people = [{
+//   id: 0,
+//   name: 'Creola Katherine Johnson',
+//   profession: 'mathematician',
+// }, {
+//   id: 1,
+//   name: 'Mario José Molina-Pasquel Henríquez',
+//   profession: 'chemist',
+// }, {
+//   id: 2,
+//   name: 'Mohammad Abdus Salam',
+//   profession: 'physicist',
+// }, {
+//   name: 'Percy Lavon Julian',
+//   profession: 'chemist',  
+// }, {
+//   name: 'Subrahmanyan Chandrasekhar',
+//   profession: 'astrophysicist',
+// }];
+
+// export default function List() {
+//   const listItems = people.map(person =>
+//     <li>{person}</li>
+//   );
+//   return <ul>{listItems}</ul>;
+// }
+
+
+// import { people } from './data.js';
+// import { getImageUrl } from './utils.js';
+
+// export default function List() {
+//   const chemists = people.filter(person =>
+//     person.profession === 'chemist'
+//   );
+//   const listItems = chemists.map(person =>
+//     <li>
+//       <img
+//         src={getImageUrl(person)}
+//         alt={person.name}
+//       />
+//       <p>
+//         <b>{person.name}:</b>
+//         {' ' + person.profession + ' '}
+//         known for {person.accomplishment}
+//       </p>
+//     </li>
+//   );
+//   return <ul>{listItems}</ul>;
+// }
+
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+export default function List() {
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+      </p>
     </li>
   );
+  return <ul>{listItems}</ul>;
 }
-
-export default function PackingList() {
-  return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item 
-          importance={9} 
-          name="Space suit" 
-        />
-        <Item 
-          importance={0} 
-          name="Helmet with a golden leaf" 
-        />
-        <Item 
-          importance={6} 
-          name="Photo of Tam" 
-        />
-      </ul>
-    </section>
-  );
-}
-
-
-
